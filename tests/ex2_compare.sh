f1063b01e1c9a705adb88dd4e18edc8a76e4cdff
Mariana Martins <mariana.martins@tum.de>
2020-04-15 17:26:17 +0200

    Fix test script

--- tests/ex2_compare.sh
+++ tests/ex2_compare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IGNORE_REGEXP="(^\s*(date|time):(.*|$)|^.*\.nml(.*|$))"
+IGNORE_REGEXP="(^\s*(date|time|mpibuflen):(.*|$)|^.*\.nml(.*|$))"
 
 DIFF=$(diff -u -B -a <(grep -vE "$IGNORE_REGEXP" <(./cmain "$1"))  <(grep -vE "$IGNORE_REGEXP" <(./LibPFASST/Tutorials/EX2_Dahlquist/main.exe "$1")))
 
