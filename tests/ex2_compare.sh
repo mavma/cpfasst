a9f0b993e4092ea0ee8023839b674e444962a3df
Mariana Martins <33549598+mavma@users.noreply.github.com>
2020-04-16 18:45:49 +0200

    Update ex2_compare.sh

--- tests/ex2_compare.sh
+++ tests/ex2_compare.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Usage: build EX2 in the LibPFASST examples, then invoke from project root with the nml file path, e.g.
+# ./tests/ex2_compare.sh probin.nml
+
 IGNORE_REGEXP="(^\s*(date|time|mpibuflen):(.*|$)|^.*\.nml(.*|$))"
 
 DIFF=$(diff -u -B -a <(grep -vE "$IGNORE_REGEXP" <(./cmain "$1"))  <(grep -vE "$IGNORE_REGEXP" <(./LibPFASST/Tutorials/EX2_Dahlquist/main.exe "$1")))
