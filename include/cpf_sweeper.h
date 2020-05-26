9c4bbebd13b51328ed40aacf574c094902be95b0
mavma <33549598+mavma@users.noreply.github.com>
2020-05-26 12:03:19 +0200

    Add initialize and destroy callbacks to sweeper

--- include/cpf_sweeper.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void sweeper_f_eval_cb(void** y, double* t, int* level_index, void** f, int* piece);
-void sweeper_f_comp_cb(void** y, double* t, double* dtq, void** rhs, int* level_index, void** f, int* piece);
