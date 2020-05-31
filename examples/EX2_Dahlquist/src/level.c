e64e094bb2530491060893fc99b21efea95859c7
mavma <33549598+mavma@users.noreply.github.com>
2020-05-31 14:03:56 +0200

    Add level index argument to restrict and interpolate

--- examples/EX2_Dahlquist/src/level.c
+++ examples/EX2_Dahlquist/src/level.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include "local.h"
 
-void level_interpolate_cb(void** f_lev, void** c_lev, void** f_vec, void** c_vec, double* t, int* flags) {
+void level_interpolate_cb(int* f_lev_idx, int* c_lev_idx, void** f_vec, void** c_vec, double* t, int* flags) {
     memcpy(*f_vec, *c_vec, sizeof(custom_data_t));
 }
-void level_restrict_cb(void** f_lev, void** c_lev, void** f_vec, void** c_vec, double* t, int* flags){
+void level_restrict_cb(int* f_lev_idx, int* c_lev_idx, void** f_vec, void** c_vec, double* t, int* flags){
     memcpy(*c_vec, *f_vec, sizeof(custom_data_t));
 }
